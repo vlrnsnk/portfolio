016943b92c902add3a8009f1f86ef135d9205744
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-20 07:37:35 -0400

    Add sass and gulp-sass dev dependencies and setup gulp task to watch and compile scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,27 @@
 
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('server', () => {
   browserSync({
     server: {
-      baseDir: ""
+      baseDir: 'src/',
     }
   });
 
-  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.parallel('server'));
+gulp.task('styles', () => {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/sass/**/*.scss', gulp.series('styles'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'server', 'watch'));
