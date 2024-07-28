e934c748d15b5bf548a6c971846dddbb98856a0b
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-27 21:05:13 -0400

    Add toggle class on hamburger-wrapper

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeMobileMenu = document.querySelector('.close-mobile-menu');
 
 const hamburgerClickHandler = () => {
   mobileMenu.classList.toggle('mobile-menu--active');
+  hamburger.classList.toggle('hamburger--cross');
+  hamburgerWrapper.classList.toggle('hamburger--active');
 };
 
 hamburger.addEventListener('click', hamburgerClickHandler);
-closeMobileMenu.addEventListener('click', hamburgerClickHandler);
