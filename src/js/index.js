7edcf7c038efac2c9d834295a0498ffb9aeed544
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-27 22:52:25 -0400

    Refactor class names of hamburger and curtain menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.main-navigation__wrapper');
 
 const hamburgerClickHandler = () => {
-  mobileMenu.classList.toggle('mobile-menu--active');
+  mobileMenu.classList.toggle('main-navigation__wrapper--active');
   hamburger.classList.toggle('hamburger--cross');
-  hamburgerWrapper.classList.toggle('hamburger--active');
+  hamburgerWrapper.classList.toggle('hamburger-wrapper--active');
 };
 
 hamburger.addEventListener('click', hamburgerClickHandler);
