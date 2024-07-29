9db4525e976d93cabb08d459ab8b55921d6e4653
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-28 22:16:58 -0400

    Add animating click on intro screen

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
 const mobileMenu = document.querySelector('.main-navigation__wrapper');
+const intro = document.querySelector('.intro');
 
 const hamburgerClickHandler = () => {
   mobileMenu.classList.toggle('main-navigation__wrapper--active');
@@ -8,4 +9,14 @@ const hamburgerClickHandler = () => {
   hamburgerWrapper.classList.toggle('hamburger-wrapper--active');
 };
 
+const introClickHandler = () => {
+  intro.style.opacity = 0;
+  intro.style.height = 0;
+
+  setTimeout(() => {
+    intro.classList.add('intro--hidden');
+  }, 500);
+}
+
 hamburger.addEventListener('click', hamburgerClickHandler);
+intro.addEventListener('click', introClickHandler);
