add449a1f2d3153fad9c357784619580639cf430
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-28 23:34:00 -0400

    Add mousemove and touchmove events to intro, make it 100% height

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ const hamburger = document.querySelector('.hamburger');
 const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
 const mobileMenu = document.querySelector('.main-navigation__wrapper');
 const intro = document.querySelector('.intro');
+const hero = document.querySelector('.hero');
+const about = document.querySelector('.about');
+const body = document.querySelector('body');
 
 const hamburgerClickHandler = () => {
   mobileMenu.classList.toggle('main-navigation__wrapper--active');
@@ -12,11 +15,18 @@ const hamburgerClickHandler = () => {
 const introClickHandler = () => {
   intro.style.opacity = 0;
   intro.style.height = 0;
+  hero.style.opacity = 1;
+  about.style.opacity = 1;
+  hamburger.style.opacity = 1;
+  body.style.height = 'auto';
+  body.style.overflow = 'auto';
 
   setTimeout(() => {
     intro.classList.add('intro--hidden');
-  }, 500);
+  }, 1000);
 }
 
 hamburger.addEventListener('click', hamburgerClickHandler);
 intro.addEventListener('click', introClickHandler);
+intro.addEventListener('mousemove', introClickHandler);
+intro.addEventListener('touchmove', introClickHandler);
