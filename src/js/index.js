26540cabb9bcc1223ae533e6afab555967e0bf8d
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-29 21:20:30 -0400

    Add tab switching functionality when clicking on tabs

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,35 @@ const intro = document.querySelector('.intro');
 const hero = document.querySelector('.hero');
 const about = document.querySelector('.about');
 const body = document.querySelector('body');
+const tabsList = document.querySelector('.projects__tabs-list');
+const tabsButtons = document.querySelectorAll('.projects__tabs-button');
+const projects = document.querySelectorAll('.project__item');
+
+// Hide all projects except for default 'react' type
+
+projects.forEach((project) => {
+  if (project.dataset.type !== 'react') {
+    project.style.display = 'none';
+  }
+});
+
+// Show projects by type of clicked tab
+
+const showProjectsByType = (type = 'all') => {
+  if (type === 'all') {
+    projects.forEach((project) => {
+      project.style.display = 'block';
+    });
+  }
+
+  projects.forEach((project) => {
+    if (project.dataset.type === type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+};
 
 const hamburgerClickHandler = () => {
   mobileMenu.classList.toggle('main-navigation__wrapper--active');
@@ -41,3 +70,14 @@ hamburger.addEventListener('click', hamburgerClickHandler);
 intro.addEventListener('click', introClickHandler);
 intro.addEventListener('mousemove', introClickHandler);
 intro.addEventListener('touchmove', introClickHandler);
+
+// Handle tabs switching
+
+tabsList.addEventListener('click', (event) => {
+  tabsButtons.forEach((tabsButton) => {
+    tabsButton.classList.remove('projects__tabs-button--active');
+  });
+
+  event.target.classList.add('projects__tabs-button--active');
+  showProjectsByType(event.target.dataset.type);
+});
