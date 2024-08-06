3781acd48d8ace710f86e1ab8240d6be5403c267
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-08-06 19:48:39 -0400

    Refactor export of variables

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,9 +1,7 @@
-const hamburger = document.querySelector('.hamburger');
-const mainNavigation = document.querySelector('.main-navigation__wrapper');
-const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
-const body = document.querySelector('body');
-const siteNavigationLinks = document.querySelectorAll('.site-navigation__link');
-const themeSwitch = document.querySelector('.theme-switch');
-const themeSwitchIcon = themeSwitch.querySelector('.theme-switch__icon use');
-
-export { hamburger, mainNavigation, hamburgerWrapper, body, siteNavigationLinks, themeSwitch, themeSwitchIcon };
+export const hamburger = document.querySelector('.hamburger');
+export const mainNavigation = document.querySelector('.main-navigation__wrapper');
+export const hamburgerWrapper = document.querySelector('.hamburger-wrapper');
+export const body = document.querySelector('body');
+export const siteNavigationLinks = document.querySelectorAll('.site-navigation__link');
+export const themeSwitch = document.querySelector('.theme-switch');
+export const themeSwitchIcon = themeSwitch.querySelector('.theme-switch__icon use');
